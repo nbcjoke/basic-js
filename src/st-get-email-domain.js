00f19764b210e7870a468217c61322095e014640
nbcjoke <79941934+nbcjoke@users.noreply.github.com>
2021-10-04 16:41:18 +0300

    Update st-get-email-domain.js

--- src/st-get-email-domain.js
+++ src/st-get-email-domain.js
@@ -8,12 +8,10 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(email) {
+export default function getEmailDomain(email) {
   let e = email;
   e = e.split('');
   const n = e.lastIndexOf('@');
   e = e.slice(n + 1);
   return e.join('');
 }
-
-module.exports = getEmailDomain;
