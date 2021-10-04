ada90a4502c1ba0045a35d1b819d3fa37d0c4ffc
nbcjoke <79941934+nbcjoke@users.noreply.github.com>
2021-10-04 17:01:43 +0300

    Update transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,34 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+export default function transform(arr) {
+  if (arr === undefined || arr.constructor != Array)
+    throw Error();
+  let transformedarr = [];
+  let flag;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == '--discard-next') {
+      if (arr[i + 1] !== undefined) {
+        i++
+        flag = true;
+      }
+    } else if (arr[i] === '--discard-prev') {
+      if (arr[i - 1] !== undefined && flag === false) {
+        transformedarr.pop()
+        flag = false;
+      }
+    } else if (arr[i] === '--double-next') {
+      if (arr[i + 1] !== undefined) {
+        transformedarr.push(arr[i + 1])
+        flag = false;
+      }
+    } else if (arr[i] === '--double-prev') {
+      if (arr[i - 1] !== undefined && flag === false)
+      transformedarr.push(transformedarr[transformedarr.length - 1])
+    } else {
+      transformedarr.push(arr[i])
+      flag = false;
+    }
+  }
+  return transformedarr;
+};
