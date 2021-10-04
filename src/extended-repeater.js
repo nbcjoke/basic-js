d9612ac81e691f765b92f0ec98b3bb1910aa079d
nbcjoke <79941934+nbcjoke@users.noreply.github.com>
2021-10-04 16:59:16 +0300

    Update extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,37 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+export default function repeater(str, options) {
+  let temp = '';
+  options.repeatTimes === undefined ? options.repeatTimes = 1 : options.repeatTimes;
+  options.separator === undefined ? options.separator = '+' : options.separator;
+  options.addition === undefined ? options.addition = '' : options.addition;
+  options.additionRepeatTimes === undefined ? options.additionRepeatTimes = 1 : options.additionRepeatTimes;
+  options.additionSeparator === undefined ? options.additionSeparator = '|' : options.additionSeparator;
+
+  function getRepeatTimes(numb, sep) {
+    let resChain = '';
+    for (let i = 0; i < numb; i++) {  
+      if(i === numb - 1) resChain += str + getAdditionRepeatTimes(options.addition, options.additionRepeatTimes, options.additionSeparator);
+
+      else resChain += str + getAdditionRepeatTimes(options.addition, options.additionRepeatTimes, options.additionSeparator) + sep;
+    }
+    return resChain;
+  }
+
+  function getAdditionRepeatTimes(add, numb, step) {
+    
+    let result = '';
+    
+    for (let i = 0; i < numb; i++) {
+      if(i === numb - 1){
+        result += add;
+      }
+      else result += add + step;
+    }
+    return result;
+  }
+  
+  return temp += getRepeatTimes(options.repeatTimes, options.separator);
+  
+};
