930e75714feb528f925e893c71711dc54ebcfe4a
nbcjoke <79941934+nbcjoke@users.noreply.github.com>
2021-10-04 17:01:13 +0300

    Update simple-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,24 +5,47 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default {
+  let chain = [];
+const chainMaker = {
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-};
+     return chain.length;
+   },
+   addLink(value) {
+     if(value === undefined) {
+       chain.push(' ');
+       return chainMaker;
+     }
+     value = '' + value;
+     chain.push(value);
+     return chainMaker;
+   },
+   removeLink(position) {
+     if(isNaN(position) || position >= chain.length || position <= 0) {
+        chain = [];
+        throw Error();
+     }
+     
+     chain.splice(position-1, 1);
+     return chainMaker;
+   },
+   reverseChain() {
+     chain.reverse();
+     return chainMaker;
+   },
+   finishChain() {
+     for(let i = 0; i < chain.length; i++) {
+       chain[i] = '( ' + chain[i] + ' )';
+       if(i !==chain.length-1) {
+         chain[i] = chain[i] + '~~';
+       }
+     }
+     let returnedValue = chain.join('');
+    chain = [];
+		return returnedValue;
+   }
+ };
+
+
+
+
